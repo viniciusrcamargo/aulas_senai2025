881a028b11dd3c98e14b7f12d1807b78ea282229
Vinicius <viniciusr.camargo@gmail.com>
2025-02-02 15:34:42 -0300

    Projeto LocalStorage aula FrontEnd

--- frontend/crud_localstorage/src/assets/crud.js
+++ frontend/crud_localstorage/src/assets/crud.js
@@ -1,9 +1,17 @@
+//Recuperando elementos do DOM
 const formContribuinte = document.getElementById('form-contribuinte');
 const nomeInput = document.getElementById('nome');
 const enderecoInput = document.getElementById('endereco');
 const cpfInput = document.getElementById('cpf');
 const telefoneInput = document.getElementById('telefone');
 const listContribuinte = document.getElementById('lista-contribuinte');
+
+function saveContribuinte(contribuinte){
+    const contribuintes = JSON.parse(localStorage.getItem('contribuintes')) || [];
+    contribuintes.push(contribuinte);
+    localStorage.setItem('contribuintes', JSON.stringify(contribuintes));
+    loadContribuintes();
+}
 
 
 
