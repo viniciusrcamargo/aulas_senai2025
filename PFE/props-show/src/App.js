2eb7371df5fe9ecc1d1acdcfa808f3b59eb01926
Vinicius <viniciusr.camargo@gmail.com>
2025-02-22 19:55:41 -0300

    Alterand projeto

--- PFE/props-show/src/App.js
+++ PFE/props-show/src/App.js
@@ -10,7 +10,7 @@ function App() {
   ];
 
   return (
-    <div>
+    <section>
      <h1>Lista Produtos</h1>
      {produtos.map((produto) => (
         <Produto nome={produto.nome} preco={produto.preco} />
