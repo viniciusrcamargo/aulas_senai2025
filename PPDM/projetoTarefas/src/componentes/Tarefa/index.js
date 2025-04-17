f015a632ae3da4ada00f703bd1ef55be19efde8e
Vinicius <viniciusr.camargo@gmail.com>
2025-04-17 14:28:52 -0300

    App de Lista de tarefas

--- PPDM/projetoTarefas/src/componentes/Tarefa/index.js
+++ PPDM/projetoTarefas/src/componentes/Tarefa/index.js
@@ -1,17 +1,58 @@
-import {View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, FlatList, StyleSheet} from 'react-native';
 import tarefas from '../../mocks/Tarefas';
 
-export default function Tarefa(){
-    const renderizaItem = ({ item }) => (
-       <View>
-            <View>
-                <Image source={item.imagem} />
-                <Text>{item.nome}</Text>
+console.log(tarefas); // Adicione esta linha para verificar os dados
+
+export default function Lista(){
+    const renderizarItem = ({item}) => (
+        <View style={estilos.item}>
+            <View style={estilos.tituloImagem}>
+                <Image source={item.imagem} style={estilos.imagem}/>
+                <Text style={estilos.titulo}>{item.nome}</Text>
             </View>
-       </View> 
+            <Text style={estilos.info}>{item.descricao}</Text>
+        </View>
     );
 
     return(
-        //
+        <View style={estilos.container}>
+            <FlatList 
+                data={tarefas}//dados
+                renderItem={renderizarItem}//função que renderiza
+                keyExtractor={item => item.id}
+            />
+        </View>
     );
 }
+
+const estilos = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        margin: 10,
+        marginTop: 15,
+    },
+    item:{
+        backgroundColor: '#9BF2EA',
+        padding: 20,
+        marginTop: 5,
+        marginVertical: 8,
+        borderRadius: 10,
+    },
+    titulo:{
+        fontSize: 24,
+        marginLeft: 10,
+    },
+    tituloImagem:{
+        flexDirection: 'row',
+    },
+    info:{
+        flexDirection: 'column',
+        fontSize: 16
+    },
+    imagem:{
+        width: 40,
+        height: 40,
+    }
+});
