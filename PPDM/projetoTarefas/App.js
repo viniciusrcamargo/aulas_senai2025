52f3e2708b3351c1107e4d1caf08b09b007a136d
Vinicius <viniciusr.camargo@gmail.com>
2025-04-23 19:52:28 -0300

    Ajustando o layout

--- PPDM/projetoTarefas/App.js
+++ PPDM/projetoTarefas/App.js
@@ -15,7 +15,7 @@ export default function App() {
 
 const estilos = StyleSheet.create({
   container: {
-    flexDirection: 'columm',
+    flexDirection: 'column',
     backgroundColor: '#fff',
   },
 });
