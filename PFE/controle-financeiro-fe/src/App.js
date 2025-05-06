8da4fbdc1af3791590d1667af2b7fc5a4bff9d18
Vinicius <viniciusr.camargo@gmail.com>
2025-05-06 15:11:25 -0300

    Projeto Financeiro com os Alunos

--- PFE/controle-financeiro-fe/src/App.js
+++ PFE/controle-financeiro-fe/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Login from "./componentes/pages/Login";
 import Register from "./componentes/pages/Register";
 import Dashboard from "./componentes/pages/Dashboard";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AdicionarMovimentacao from "./componentes/pages/Movimentacoes/Adicionar";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Login/>} />
         <Route path="/registrar" element={<Register/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/addmovimentacao" element={<AdicionarMovimentacao/>} />
       </Routes>
     </Router>
   );
