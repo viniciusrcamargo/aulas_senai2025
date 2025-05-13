c827e59bc152f514e934802f239972a36e194e81
Vinicius <viniciusr.camargo@gmail.com>
2025-05-13 11:08:34 -0300

    Projeto Controle Financeiro

--- PFE/controle-financeiro-fe/src/App.js
+++ PFE/controle-financeiro-fe/src/App.js
@@ -5,16 +5,18 @@ import Login from "./componentes/pages/Login";
 import Register from "./componentes/pages/Register";
 import Dashboard from "./componentes/pages/Dashboard";
 import AdicionarMovimentacao from "./componentes/pages/Movimentacoes/Adicionar";
+import RelatorioMovimentacoes from "./componentes/pages/Movimentacoes/Relatorio";
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/" element={<Login />} />
         <Route path="/registrar" element={<Register/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/addmovimentacao" element={<AdicionarMovimentacao/>} />
+        <Route path="/relatoriomovimentacao" element={<RelatorioMovimentacoes/>} />
       </Routes>
     </Router>
   );
