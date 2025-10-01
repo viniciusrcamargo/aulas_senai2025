a570f0e7fc30d185fda44e53bc8771a807044060
Vinicius <Viniciusr.camargo@gmail.com>
2025-10-01 14:05:40 -0300

    Finalizando a tela de login

--- PPDM/controleFinanceiroApp/app/index.jsx
+++ PPDM/controleFinanceiroApp/app/index.jsx
@@ -1,15 +1,7 @@
-import { Text, View } from "react-native";
+import Navigation from './navigation'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Navigation/>
   );
 }
